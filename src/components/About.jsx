d128fddf46c995920a345b3f31c192d3f0c55c5e
Katelyn <katelyncmiles@gmail.com>
2020-02-04 15:37:47 -0500

    created about style page

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,10 +1,15 @@
 import React from 'react'
+import '../styles/about.css'
 
 const About = () => {
   return(
     <div className = "about">
 			<h1>About</h1>
-			<h2>Brand Statement</h2>
+			<h2>Brand Statement (NYC based vocal group etc...)</h2>
+			<h1>Members</h1>
+			<div className = "members">
+				<h3>There will be a grid of individual headshots in this space</h3>
+			</div>
 		</div> 
   )
 }
