92b9d6dc8ffff9c535465a41d3388a9e16093a23
Katelyn <katelyncmiles@gmail.com>
2020-02-04 15:26:37 -0500

    added about component

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import './App.css';
 import Header from './components/Header'
 import Nav from './components/Nav'
+import About from './components/About'
 
 function App() {
   return (
     <div className="App">
       <Nav/>
       <Header/>
+      <About/>
     </div>
   );
 }
